22cea0c6f7f04cc792d99baff4cf415ad3f6756a
Natalia <ngaik1@sheffield.ac.uk>
2021-02-09 21:54:48 +0100

    first commit
    first commit

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # weekly_calendar_app
 
-In  weekly calendar app, a user can add, delete, and edit day to day events within a week. 
+## Project setup
+```
+yarn install
+```
 
-Each event corresponds to a particular task/to-do item that the user would like to keep track of.
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+      <CalendarWeek/>
+        <CalendarEntry/>
+    </div>
+</template>
+
+<script>
+    import CalendarWeek from "@/components/CalendarWeek";
+    import CalendarEntry from "@/components/CalendarEntry";
+    export default {
+        name: 'App',
+        components: {
+            CalendarEntry,
+            CalendarWeek
+        }
+    }
+</script>
+
+<style lang="scss">
+    html, body, #app {
+        height: 100%;
+    }
+</style>
+
+<style lang="scss" scoped>
+    #app {
+        background: #6e6e6e;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        -webkit-align-items: center;
+        justify-content: center;
+        -webkit-justify-content: center;
+    }
+</style>
